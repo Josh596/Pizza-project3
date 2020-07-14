a200f1125a92abef516368648748d7a8b3ad0beb
Josh596 <64589157+Josh596@users.noreply.github.com>
2020-07-14 21:06:03 +0100

    Modal seems nice.

--- orders/static/orders/index.js
+++ orders/static/orders/index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     
     //registering the modals and constant variables
     const modal = document.querySelector('.modals');
-    const closeButton = document.querySelector('.close-button')
+    let closeButton = document.querySelector('.close-button')
 
 
     //For displaying the modals
@@ -27,20 +27,51 @@ document.addEventListener('DOMContentLoaded', () =>{
             console.log(item)
             //Load toggleModal
             toggleModal();
+            //time to send the ajax requests
             let request = new XMLHttpRequest
             request.open('GET', `/${item}`)
             
             request.send()
 
             request.onload = () =>{
-                console.log(request.response['item_price'])
+                let json_data = request.response
+                //Getting data from the ajax request
+                data = JSON.parse(json_data)
+                console.log(data)
+                let price = []
+                for(info in data){
+                    if('price' in data[info]['fields']){
+                        item_price = data[info]['fields']['price']
+                        //
+                        price.push(item_price)                    
+                                                        }                    
+                                }
+
+                console.log(price)
+                small_price = Math.min.apply(null, price)
+                large_price = Math.max.apply(null, price)
+                //Checking for small and large price
+                if(small_price != large_price){
+                    console.log(small_price)
+                    console.log(large_price)
+                }
+                else{
+                    console.log(small_price)
+                }
+
+
             }
             
         })
     })
 
-    closeButton.addEventListener("click", toggleModal);
-    window.addEventListener("click", windowOnClick);
+    //Sample output
+   // [{"model": "orders.items", "pk": 1, "fields": {"menu": 1, "name": "Regular Cheese Pizza"}}, 
+    //{"model": "orders.item_price", "pk": 1, "fields": {"item": 1, "price": "12.70", "size": 1}}, 
+   // {"model": "orders.item_price", "pk": 2, "fields": {"item": 1, "price": "17.95", "size": 2}}]
+
+    closeButton.addEventListener("click", toggleModal)
+    document.addEventListener("click", windowOnClick);
 
 
 
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+import json
 
 
 #Importing my models
@@ -14,10 +16,13 @@ def index(request):
 
 def get_item(request, item_name):
     #Using ajax requests
-    if request.method == 'GET':
-
+    if request.method == 'GET' and request.is_ajax:
+        
         item_data = Items.objects.get(name = item_name)
         print(item_data)
+
+        pizza_menu = Menu.objects.get(name = 'Pizza')
+        if item_data.name in pizza_menu
 
         item_price = item_data.price.all()
         print(Item_price)
@@ -25,13 +30,18 @@ def get_item(request, item_name):
         add_on = item_data.add_on.all()
         print(add_on)
 
+        size = item_data.price.all()
+
         context = {
             'item_price': item_price,
             'add_on': add_on
         }
         print(context)
+        context_json = serializers.serialize("json", [item_data, *item_price, *add_on])
         
-        return HttpResponse(context)
+
+        
+        return HttpResponse(context_json, content_type ="application/json")
     else:
         return HttpResponse('unsuccessful')
 # Problem with models... item not recognixed orders.models.Items.DoesNotExist: Items matching query does not exist.
