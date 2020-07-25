e18fbb571e880df2c7cdef2539e69a572cce4895
Josh596 <64589157+Josh596@users.noreply.github.com>
2020-07-25 16:06:47 +0100

    Wrapping up

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 from django.core import serializers
-import json
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
+import json
 #Importing my models
 from .models import *
 
@@ -37,19 +36,20 @@ def login_view(request):
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
+            request.session['username'] = username
             return HttpResponseRedirect(reverse(index))
         else:
             return render(request, "orders/register.html")
     else:
         return render(request, "orders/login.html")
+#Making menu_name and item_data a global variable
 
 def get_item(request, item_details):
     #Using ajax requests
     if request.method == 'GET' and request.is_ajax:
-        
+
         #Getting the menu the item belongs too
         #converting item_details to string from json
-        print('hello')
         item_details_str = json.loads(item_details)
         menu_name = Menu.objects.get(name = item_details_str['menu_name'])
 
@@ -74,16 +74,39 @@ def get_item(request, item_details):
         }
         print(context)
         context_json = serializers.serialize("json", [item_data, *item_price, *add_on, *topping])
-        
-
-        
+          
         return HttpResponse(context_json, content_type ="application/json")
-    else:
-        return HttpResponse('unsuccessful')
+  
+    return Http404("Page not found")
+    
 
 def order(request):
     if request.method == 'POST':
-        pass
+        size = request.POST['size']
+        toppings = request.POST.getlist('toppings', None)
+        item_id = request.POST['item_id']
+        menu_id = request.POST['menu_id']
+
+        menu = Menu.objects.get(pk = menu_id)
+        item_size = Size.objects.get(size = size)
+        size_id = item_size.pk
+        item = menu.item.get(pk = item_id)
+       
+
+        item_price = item.price.get(size = size_id)
+        main_price = item_price.price
+        price = 0
+        for topping in toppings:
+          price += 0.5
+        
+       
+        
+        
+        return HttpResponse(main_price)
+      
+    
+
+
     else:
         return Http404("Page does not exist")
 
